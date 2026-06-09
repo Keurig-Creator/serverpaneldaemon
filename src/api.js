450eb63360699be84ca95bca2c29668ecc838af4
David Clark <threetwoone112@gmail.com>
2026-06-08 21:02:16 -0500

    port change

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3002;
+const port = process.env.PORT || 3000;
 const daemon = require('./daemon');
 
 app.use(express.json());
